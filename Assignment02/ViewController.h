6275a93b6f3b1c7231b41645c9ac4944acc8f533
Khaled <khaledalyousefi@Khaleds-MacBook-Pro.local>
2013-10-02 12:12:25 -0700

    Initial Commit

---
+++ Assignment02/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Assignment02
+//
+//  Created by Khaled Alyousefi on 10/2/13.
+//  Copyright (c) 2013 Khaled Alyousefi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
